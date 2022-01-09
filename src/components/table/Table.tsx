0a10b3a6692cdaa667d62173090da2bc3f078325
samuel <samuel.jordan.dobbie@gmail.com>
2022-01-09 19:50:43 +0000

    Add build shelf form + data table for explorer

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,103 +1,91 @@
-// import { useEffect, useState } from "react"
-// import { MDBDataTable } from "mdbreact"
+import { useState } from "@hookstate/core"
+import { CircularProgress, Typography } from "@mui/material"
+import MUIDataTable from "mui-datatables"
+import { useEffect } from "react"
 
-// import Endpoint from "@shelf/commons/Endpoint"
+const columns = [
+  "Created",
+  "Title",
+  "# Resources",
+  "# Views",
+]
 
-// import "./Table.css"
+function Table(): JSX.Element {
+  const load = useState(false)
+  const shelves = useState([] as string[][])
 
-// const columns = [
-//   {
-//     label: "created",
-//     field: "created",
-//     width: 100,
-//   },
-//   {
-//     label: "title",
-//     field: "title",
-//     width: 300,
-//   },
-//   {
-//     label: "# resources",
-//     field: "resources_size",
-//     width: 100,
-//   },
-//   {
-//     label: "# views",
-//     field: "views",
-//     width: 100,
-//     sort: "desc",
-//   },
-// ]
+  useEffect(() => {
+    shelves.set([
+      ["2020-01-01", "My first shelf", "1", "1"],
+      ["2020-01-01", "My first shelf", "1", "1"],
+      ["2020-01-01", "My first shelf", "1", "1"],
+    ])
+  }, [])
 
-// function Table(): JSX.Element {
-//   const [load, setLoad] = useState(false)
-//   const [shelves, setShelves] = useState({
-//     columns: columns,
-//     rows: [],
-//   })
+  // useEffect(() => {
+  //   setLoad(true)
 
-//   useEffect(() => {
-//     setLoad(true)
+  //   const url = `${Endpoint.Server.Shelf}/all`
 
-//     const url = `${Endpoint.Server.Shelf}/all`
+  //   fetch(url)
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       if (data.statusCode === 200) {
+  //         const rows = JSON.parse(data.message)
 
-//     fetch(url)
-//       .then(response => response.json())
-//       .then(data => {
-//         if (data.statusCode === 200) {
-//           const rows = JSON.parse(data.message)
+  //         for (let i = 0; i < rows.length; i++) {
+  //           const created = rows[i]["created"]
+  //           const id = rows[i]["_id"]
 
-//           for (let i = 0; i < rows.length; i++) {
-//             const created = rows[i]["created"]
-//             const id = rows[i]["_id"]
+  //           rows[i]["created"] = getDate(created)
+  //           rows[i]["clickEvent"] = () => redirectToShelf(id)
+  //         }
 
-//             rows[i]["created"] = getDate(created)
-//             rows[i]["clickEvent"] = () => redirectToShelf(id)
-//           }
+  //         setShelves({
+  //           columns: columns,
+  //           rows: rows,
+  //         })
+  //       }
+  //     })
+  //     .finally(() => setLoad(false))
+  // }, [])
 
-//           setShelves({
-//             columns: columns,
-//             rows: rows,
-//           })
-//         }
-//       })
-//       .finally(() => setLoad(false))
-//   }, [])
+  return (
+    <>
+      {load.value &&
+        <>
+          <CircularProgress />
+          
+          <Typography>
+            Loading shelves...
+          </Typography>
+        </>
+      }
 
-//   return (
-//     <>
-//       {load &&
-//         <div className="text-center">
-//           <div className="spinner-border" role="status"></div>
-//           <p className="spinner-text">loading shelves...</p>
-//         </div>
-//       }
+      {!load.value &&
+        <MUIDataTable
+          title="All Shelves"
+          data={shelves.value}
+          columns={columns}
+        />
+      }
+    </>
+  )
+}
 
-//       {!load &&
-//         <MDBDataTable
-//           data={shelves}
-//           hover
-//         />
-//       }
-//     </>
-//   )
-// }
+function redirectToShelf(shelfId: string): void {
+  window.open(`${window.location.origin}/s/${shelfId}`)
+}
 
-// function redirectToShelf(shelfId: string): void {
-//   window.open(`${window.location.origin}/s/${shelfId}`)
-// }
+function getDate(epoch: number): string {
+  const date = new Date(0)
+  date.setUTCSeconds(epoch)
 
-// function getDate(epoch: number): string {
-//   const date = new Date(0)
-//   date.setUTCSeconds(epoch)
+  const day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(date)
+  const month = new Intl.DateTimeFormat("en", { month: "short" }).format(date)
+  const year = new Intl.DateTimeFormat("en", { year: "2-digit" }).format(date)
 
-//   const day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(date)
-//   const month = new Intl.DateTimeFormat("en", { month: "short" }).format(date)
-//   const year = new Intl.DateTimeFormat("en", { year: "2-digit" }).format(date)
+  return `${day}-${month}-${year}`.toLowerCase()
+}
 
-//   return `${day}-${month}-${year}`.toLowerCase()
-// }
-
-// export default Table
-
-export {}
+export default Table
